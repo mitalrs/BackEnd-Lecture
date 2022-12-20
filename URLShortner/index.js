05ddc9d01fbf3fdb9822ef98b2ab00d5154c84be
mitalrs <mitalsapkale1999@gmail.com>
2022-12-20 20:07:14 +0530

    set middleware

--- URLShortner/index.js
+++ URLShortner/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-//middlewares
+//middlewares: The role of middleware is to enable and ease access to those back-end resources. Middleware programs will typically provide a messaging service for applications to transfer data, such as simple object access protocol (SOAP), representational state transfer (REST) or JavaScript object notation (JSON).
+//middleware function that called before making the API call
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = 2020;
 
