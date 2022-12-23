61a6bb26ff55b9998c30cc55dbed785aa5775c92
mitalrs <mitalsapkale1999@gmail.com>
2022-12-23 22:56:01 +0530

    finding a error in started a server

--- URLShortner/index.js
+++ URLShortner/index.js
@@ -3,9 +3,9 @@ const app = express();
 
 //middlewares: The role of middleware is to enable and ease access to those back-end resources. Middleware programs will typically provide a messaging service for applications to transfer data, such as simple object access protocol (SOAP), representational state transfer (REST) or JavaScript object notation (JSON).
 //middleware function that called before making the API call
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // it's basically accept application/json for express server
+app.use(express.static("public")); // we are put ower html in to public folder
+app.use(express.urlencoded({ extended: false })); //u can accept req.body
 
 const PORT = 2020;
 
