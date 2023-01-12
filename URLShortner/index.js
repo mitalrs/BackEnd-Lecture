42a6faf05c4ec4f6f69c45b783cb1d234281bf45
mitalrs <mitalsapkale1999@gmail.com>
2023-01-12 11:04:34 +0530

    added route folder and index.html

--- URLShortner/index.js
+++ URLShortner/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const shorturl = require('./routers/url')
+const homeRoutes = require('./routers/home')
 
 //middlewares: The role of middleware is to enable and ease access to those back-end resources. Middleware programs will typically provide a messaging service for applications to transfer data, such as simple object access protocol (SOAP), representational state transfer (REST) or JavaScript object notation (JSON).
 //middleware function that called before making the API call
@@ -7,7 +9,10 @@ app.use(express.json()); // it's basically accept application/json for express s
 app.use(express.static("public")); // we are put ower html in to public folder
 app.use(express.urlencoded({ extended: false })); //u can accept req.body
 
-const PORT = 2020;
+const PORT = 1337;
+
+app.use('/urlapi',shorturl);
+app.use('/', homeRoutes);
 
 app.listen(POST,()=>{
     console.log("app is runnig on port: ", PORT);
