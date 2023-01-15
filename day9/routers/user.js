ca28c50cce9ee9a327f4bce1eaab474eac323c56
mitalrs <mitalsapkale1999@gmail.com>
2023-01-16 01:13:45 +0530

    signin done with jwt added

--- day9/routers/user.js
+++ day9/routers/user.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const{
     validateName,
@@ -31,7 +32,7 @@ router.post("/signup", async (req, res)=>{
             return res.status(400).json({ err: "password validation fails"})
         }
 
-        const hashPassword = await bcrypt.hash(password);
+        const hashPassword = await bcrypt.hash(password, (saltOrRounds = 10));
         const user = {
             email,
             name,
@@ -50,6 +51,47 @@ router.post("/signup", async (req, res)=>{
     }
 });
 
+router.post('/signin', async(req, res)=>{
+    try{
+        const { email, password } = req.body;
+        if(email.length === 0){
+            return res.status(400).json({
+                err: "provide right email"
+            })
+        }
+        if(password.length === 0){
+            return res.status(400).json({
+                err: "provide right password"
+            })
+        }
+
+        const existingUser = await User.findOne({ where: { email }});
+        if(!existingUser){
+            return res.status(404).json({
+                err: "User not found"
+            })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, existingUser.password)
+        if(!passwordMatch){
+            return res.status(400).json({
+                err: "email or pass mismatched"
+            })
+        }
+
+        const payload = { user: { id: existingUser.id }};
+        const bearertoken = await jwt.sign(payload, "SECRET MESSAGE", {
+            expiresIn: 360000,
+        });
+        res.cookie('t', bearertoken, { expire: new Date() + 9999 })
+
+        return res.status(200).json({
+            bearertoken
+        })
+    }catch(e){
+        return res.status(500).send(e);
+    }
+})
 module.exports = router;
 
 
